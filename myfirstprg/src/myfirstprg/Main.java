ab469e994d1ef96e4c899a75f6b524c8f510a134
brronma <brronma@gmail.com>
2020-04-02 20:30:58 -0400

    2nd release

--- myfirstprg/src/myfirstprg/Main.java
+++ myfirstprg/src/myfirstprg/Main.java
@@ -5,6 +5,9 @@ class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
 	}
+	
 
 }
